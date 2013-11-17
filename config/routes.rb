e9305d93fdab2b8313f79689eeaffb97579053fb
Justin Coyne <justin@curationexperts.com>
2013-11-16 22:19:48 -0600

    Allow sizes with pixel values. Fixes #6

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Riiif::Engine.routes.draw do
   ALLOW_DOTS ||= /[\w.]+/
-  PERCENTAGE ||= /(pct:)?[\w.]+/
+  SIZES ||= /(pct:)?[\w.,]+/
   get "/:id/:region/:size/:rotation/:quality(.:format)" => "images#show", 
-    constraints: { rotation: ALLOW_DOTS, size: PERCENTAGE}
+    constraints: { rotation: ALLOW_DOTS, size: SIZES}
   get "/:id/info(.:format)" => "images#info", :constraints => { :format => /json/}
   get "/:id/view(.:format)" => "images#view"
 end
--- spec/routing/resize_routes_spec.rb
+++ spec/routing/resize_routes_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "routes" do
   routes { Riiif::Engine.routes }
 
-  describe 'for mogrification' do
+  describe 'for conversion' do
     it "routes GET /abcd1234/full/full/0/native.jpg" do
       expect(
         get: "/abcd1234/full/full/0/native.jpg"
@@ -12,11 +12,18 @@ describe "routes" do
                     quality: 'native', format: 'jpg')
     end
 
-    it "routes requests with dots" do
+    it "routes requests with floating point percent size" do
       expect(
         get: "/abcd1234/full/pct:12.5/22.5/native.jpg"
       ).to route_to(controller: "riiif/images", id: 'abcd1234', action: "show", 
                     region: 'full', size: 'pct:12.5', rotation: '22.5', 
+                    quality: 'native', format: 'jpg')
+    end
+    it "routes requests with pixel size" do
+      expect(
+        get: "/abcd1234/full/100,50/22.5/native.jpg"
+      ).to route_to(controller: "riiif/images", id: 'abcd1234', action: "show", 
+                    region: 'full', size: '100,50', rotation: '22.5', 
                     quality: 'native', format: 'jpg')
     end
   end
