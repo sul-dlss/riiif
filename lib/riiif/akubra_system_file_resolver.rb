1819b69e175fef1013da61fa898aad9db81152d7
Eric James <eric.james@yale.edu>
2015-03-10 09:40:55 -0400

    added akubra test

--- lib/riiif/akubra_system_file_resolver.rb
+++ lib/riiif/akubra_system_file_resolver.rb
@@ -1,30 +1,33 @@
 require 'digest'
 require 'cgi'
 module Riiif
-  module AkubraSystemFileResolver
-    mattr_accessor :pathroot, :imagetype, :akubraconfig
-    self.pathroot = "/yourfedora/data/datastreamStore/"
-    self.imagetype = "jp2"
-    self.akubraconfig = [[0,2],[2,2],[4,1]]
+  class AkubraSystemFileResolver
+    attr_accessor :pathroot, :imagetype, :akubraconfig
 
-    def self.find(id)
+  def initialize(pr="/yourfedora/data/datastreamStore/",ir="jp2",ac=[[0,2],[2,2],[4,1]])
+    @pathroot = pr
+    @imagetype = ir
+    @akubraconfig = ac
+  end
+
+    def find(id)
       Riiif::File.new(path(id))
     end
 
-    def self.path(id)
+    def path(id)
       search = pattern(id)
       Dir.glob(search).first || raise(ImageNotFoundError, search)
     end
 
-    def self.pattern(id)
-      fullpid = "info:fedora/#{id}/#{imagetype}/#{imagetype}.0"
+    def pattern(id)
+      fullpid = "info:fedora/#{id}/#{@imagetype}/#{@imagetype}.0"
       md5 = Digest::MD5.new
       md5.update fullpid
       digest = md5.hexdigest
       directorystr = ""
-      akubraconfig.each { |a| directorystr << digest[a[0],a[1]] << "/" }
+      @akubraconfig.each { |a| directorystr << digest[a[0],a[1]] << "/" }
       filename = CGI.escape(fullpid)
-      fullpath = pathroot + directorystr + filename
+      fullpath = @pathroot + directorystr + filename
       fullpath	  
     end
   end
---
+++ spec/models/akubra_system_file_resolver_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Riiif::AkubraSystemFileResolver do
+  subject { Riiif::AkubraSystemFileResolver.new(Rails.root.join('../../spec/samples/'),'jp2',[[0,2],[2,2],[4,1]]) }
+  it "should raise an error when the file isn't found" do
+    expect{subject.find('demo:2')}.to raise_error Riiif::ImageNotFoundError
+  end
+
+  it "should get the jpeg2000 file" do
+    expect(subject.find('demo:1').path).to eq Riiif::File.new(Dir.glob(subject.pathroot + '22/7e/9/info%3Afedora%2Fdemo%3A1%2Fjp2%2Fjp2.0').first).path
+  end
+
+  #it "should accept ids with dashes" do
+  #  subject.pattern('foo-bar-baz')
+  #end
+  #it "should accept ids with colins" do
+  #  subject.pattern('fo:baz')
+  #end
+end
