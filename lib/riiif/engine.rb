31fdc43252a05f9ecace7aaaf9782550782d3ba7
Justin Coyne <jcoyne@justincoyne.com>
2017-09-08 10:53:02 -0500

    Handle ImageNotFound error with the :not_found handler
    Fixes #101

--- lib/riiif/engine.rb
+++ lib/riiif/engine.rb
@@ -4,5 +4,9 @@ module Riiif
 
     # How long to cache the tiles for.
     config.cache_duration_in_days = 3
+
+    config.action_dispatch.rescue_responses.merge!(
+      'Riiif::ImageNotFoundError' => :not_found
+    )
   end
 end
