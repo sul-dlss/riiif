9e934d18321010f9937393bf7ca9c4287ad756c9
Chris Beer <cabeer@stanford.edu>
2024-09-09 16:34:24 -0700

    Make zeitwerk ok with our test app generator

--- lib/riiif/engine.rb
+++ lib/riiif/engine.rb
@@ -10,5 +10,15 @@ module Riiif
 
     # Set to true to use kdu for jp2000 source images
     config.kakadu_enabled = false
+
+    config.before_configuration do
+      # see https://github.com/fxn/zeitwerk#for_gem
+      # We put a generator into LOCAL APP lib/generators, so tell
+      # zeitwerk to ignore the whole directory? If we're using a recent
+      # enough version of Rails to have zeitwerk config
+      #
+      # See: https://github.com/cbeer/engine_cart/issues/117
+      Rails.autoloaders.main.ignore(Rails.root.join('lib', 'generators')) if Rails.try(:autoloaders).try(:main).respond_to?(:ignore)
+    end
   end
 end
